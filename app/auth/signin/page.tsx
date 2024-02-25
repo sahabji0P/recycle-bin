e36930d163a8d27e8173128fd55336cb54854c21
Shashwat Jain <kcapshashwat@gmail.com>
2024-02-26 01:07:02 +0530

    Some editing changes in SignUP Design.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,26 +1,23 @@
-
-
 export default function SignInPage() {
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
-            <main className=" flex min-h-screen w-full flex-1 flex-col items-center justify-center bg-base-200 px-8 text-center">
+        <div className="flex h-screen flex-col items-center justify-center py-2 ">
+            <main className=" flex h-screen w-full flex-1 flex-col items-center justify-center bg-base-200 px-8 text-center">
                 <div className="mt-28 flex w-full max-w-4xl flex-col rounded-2xl shadow-2xl md:mt-16 md:w-2/3 md:flex-row ">
                     <div className="w-full rounded-t-2xl bg-base-content/10 p-5 md:w-3/5 md:rounded-l-2xl md:rounded-r-none">
                         <div className="text-left font-bold">
-                            <span className="mr-1 font-bold text-accent">Sign</span>UP
+                            <span className="mr-1 font-bold text-primary">Sign</span>UP
                         </div>
 
                         <div className="py-10">
-                            <span className="mb text-3xl font-bold text-accent">
+                            <span className="mb text-3xl font-bold text-primary">
                                 Recycle</span> <h2 className="mb text-3xl font-bold">Bin</h2>
 
 
-                            <div className="mb-2 inline-block w-12 border-2 border-accent"></div>
+                            <div className="mb-2 inline-block w-12 border-2 border-primary"></div>
 
                             <div className="flex flex-col items-center rounded">
                                 <div className="mb-3 flex w-64 items-center rounded-3xl bg-gray-600 p-2 ">
-                                    {/* <FaRegEnvelope className="mr-2 text-white" /> */}
                                     <input
                                         type="email"
                                         name="email"
@@ -30,7 +27,7 @@ export default function SignInPage() {
                                 </div>
 
                                 <div className="mb-3 flex w-64 items-center rounded-3xl bg-gray-600 p-2">
-                                    {/* <MdLockOutline className="mr-2 text-white" /> */}
+
                                     <input
                                         type="password"
                                         name="password"
@@ -48,7 +45,7 @@ export default function SignInPage() {
                                         Forgot Password?
                                     </a>
                                 </div>
-                                <a href="#" className="btn btn-outline btn-accent btn-wide">
+                                <a href="#" className="btn btn-outline btn-primary btn-wide">
                                     Sign In
                                 </a>
                             </div>
@@ -57,12 +54,12 @@ export default function SignInPage() {
                     {/*sign in section*/}
 
                     <div className="mb-24 w-full rounded-b-2xl bg-base-100 px-12 py-36 md:mb-0 md:w-2/5 md:rounded-bl-none md:rounded-br-2xl md:rounded-tr-2xl">
-                        <h2 className="mb-2 text-2xl font-bold">Hello Innovator!</h2>
+                        <h2 className="mb-2 text-2xl font-bold">Greetings!</h2>
                         <div className="mb-2 inline-block w-12 border-2 border-white"></div>
-                        <p className="mb-10 text-accent">Fill up the Details.</p>
+                        <p className="mb-10 text-primary">Tell us about yourself!</p>
                         <p className="mb-10">
-                            Find, Learn, and Innovate with{" "}
-                            <span className="text-accent">Project Finder</span>.
+                            Reduce, Reuse, and Recycle with{" "}
+                            <span className="text-primary">Recycle Bin</span>.
                         </p>
                     </div>
                 </div>
